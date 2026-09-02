1f0d9a2c537be8dc5c498f13681b9f23369da408
cbuelt <cbuelt@pm.me>
2026-09-02 13:49:42 +0000

    Deploying to gh-pages from @ cbuelt/cbuelt.github.io@a68d1d2de6d6539ca84641c932e0ebfe57824593 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/cv/CV.pdf";
+            window.location.href = "/assets/pdf/cv/cv-website.pdf";
           },
         },{id: "post-a-post-with-plotly-js",
         
